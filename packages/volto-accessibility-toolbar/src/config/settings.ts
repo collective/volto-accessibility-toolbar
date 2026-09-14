7884c513c870fedaa9fa700d298df0a5b3edce9f
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2026-09-14 21:57:50 +0200

    Updated the settings file for this add-on

--- packages/volto-accessibility-toolbar/src/config/settings.ts
+++ packages/volto-accessibility-toolbar/src/config/settings.ts
@@ -7,7 +7,7 @@ import type { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
   // Enable or disable the accessibility toolbar features
-  config.settings.enable_link = false;
+  config.settings.enable_link = true;
   // URL of the accessibility page
   config.settings.link_url = '/accessibility';
   // Enable or disable the font size feature
